1c00c66060bf6b17a3497d49229a3f9174c43281
Your Name <you@example.com>
2022-10-12 16:20:37 -0500

    asincrono

---
+++ callback/1_simple.js
@@ -0,0 +1,32 @@
+const xmlHttpRequest = require('xmlhttprequest').XMLHttpRequest
+
+const url = "https://pokeapi.co/api/v2/type"
+
+//funcion para conectar a una api 
+//en modo asincrono
+function get_data(endpoint, exito, fallo){
+    //1. crear el objeto xmlhttp
+    const h = new xmlHttpRequest();
+    //2. establecer una conexion a la api
+    h.open('GET', endpoint)
+    //3. eviar la request a un server(API)
+    h.send()
+    //4. tratamiento de datos de la response
+    h.onload = function(){
+        exito(h.responseText)
+    }
+}
+//funcion callback: exito
+function exito(response){
+    const tipos = JSON.parse(response).results
+    tipos.forEach(element => {
+        console.log(`Tipo: ${element.name}`)
+        console.log('---------------------')
+    });
+}
+//funcion callback: fallo
+function fallo(status){
+    console.log(status)
+}
+//invocar la funcion
+get_data(url, exito, fallo)
---
+++ promises/2_simple_arrow.js
@@ -0,0 +1,43 @@
+const xmlHttpRequest = require('xmlhttprequest').XMLHttpRequest
+
+const url = "https://pokeapi.co/api/v2/type"
+
+//funcion para conectar a una api 
+//en modo asincrono
+const get_data = (endpoint)=>{
+    //definir la promesa:
+    let promise = new Promise((resolve, rejected)=>{
+        //1. crear el objeto xmlhttp
+        const h = new xmlHttpRequest();
+         //2. establecer una conexion a la api
+        h.open('GET', endpoint)
+        //3. eviar la request a un server(API)
+        h.send()
+        //4. tratamiento de datos de la response
+        h.onload = ()=>{
+            if(h.status === 200){
+                resolve(h.responseText)
+            }else{
+                rejected(h.status)
+            }
+        }
+    })
+    return promise
+    
+}
+//funcion callback: exito
+const exito =(response)=>{
+    const tipos = JSON.parse(response).results
+    tipos.forEach(element => {
+        console.log(`Tipo: ${element.name}`)
+        console.log('---------------------')
+    });
+}
+//funcion callback: fallo
+const fallo =(status)=>{
+    console.log(status)
+}
+//invocar la funcion
+get_data(url).then((data)=>{
+    exito(data)
+})
